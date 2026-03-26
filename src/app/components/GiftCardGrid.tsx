28a9130c59367013189cdd7382d829cc7d6e6212
glocallink <master@glocallink-kr.com>
2026-03-26 19:45:59 +0700

    UI: Add top margin to GiftCardGrid for better vertical spacing

--- src/app/components/GiftCardGrid.tsx
+++ src/app/components/GiftCardGrid.tsx
@@ -28,7 +28,7 @@ const giftCards = [
 
 export function GiftCardGrid({ onSelectCard }: { onSelectCard?: (cardId: string) => void }) {
   return (
-    <section className="px-4 relative z-10" style={{ marginTop: "0" }}>
+    <section className="px-4 relative z-10" style={{ marginTop: "2.5rem" }}>
       <div className="max-w-lg mx-auto flex flex-col gap-3">
         {giftCards.map((card) => (
           <button
